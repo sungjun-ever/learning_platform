a5cc8d1365f931eee2894e9372eb3ce9814294c1
sungjun-ever <zkfmak10@gmail.com>
2025-10-24 16:44:06 +0900

    RoleInterface 맵핑

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,7 +6,9 @@ use App\Repositories\CompanyProfile\CompanyProfileRepository;
 use App\Repositories\CompanyProfile\ICompanyProfileRepository;
 use App\Repositories\IndividualProfile\IIndividualProfileRepository;
 use App\Repositories\IndividualProfile\IndividualProfileRepository;
+use App\Repositories\User\IRoleRepository;
 use App\Repositories\User\IUserRepository;
+use App\Repositories\User\RoleRepository;
 use App\Repositories\User\UserRepository;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,6 +22,7 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(IUserRepository::class, UserRepository::class);
         $this->app->bind(IIndividualProfileRepository::class, IndividualProfileRepository::class);
         $this->app->bind(ICompanyProfileRepository::class, CompanyProfileRepository::class);
+        $this->app->bind(IRoleRepository::class, RoleRepository::class);
     }
 
     /**
